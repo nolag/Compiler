b3474116f588611cf3976974814f51e312fc33a7
Arturo Pie <arturotd08@yahoo.ca>
2013-01-16 17:53:40 -0500

    removes trailing whitespace in NFA.java

--- Generator/src/cs444/nfa/NFA.java
+++ Generator/src/cs444/nfa/NFA.java
@@ -12,82 +12,82 @@ import cs444.nfa.transition.RangeTransition;
 public class NFA {
 
     public static NFA union(NFA... nfas) {
-        
+
         NFA result = new NFA();
-        
+
         NFAState start = result.getStartState();
         NFAState accepting = result.getAcceptingState();
-        
+
         for (NFA nfa : nfas) {
             result.addAllStates(nfa);
             start.addTransition(new EpsilonTransition(nfa.getStartState()));
             nfa.getAcceptingState().addTransition(new EpsilonTransition(accepting));
         }
-        
+
         return result;
     }
-    
+
     public static NFA concatenate(NFA... nfas) {
-        
+
         NFA result = new NFA();
-        
+
         if (nfas.length > 0) {
-        
+
             result.addAllStates(nfas[0]);
             NFAState start = result.getStartState();
             start.addTransition(new EpsilonTransition(nfas[0].getStartState()));
-        
+
             NFA previousNFA = nfas[0];
             for (int i = 1; i < nfas.length; i++) {
-                
+
                 NFA currentNFA = nfas[i];
                 result.addAllStates(currentNFA);
                 previousNFA.getAcceptingState().addTransition(new EpsilonTransition(currentNFA.getStartState()));
                 previousNFA = currentNFA;
             }
-            
+
             previousNFA.getAcceptingState().addTransition(new EpsilonTransition(result.getAcceptingState()));
         }
-        
+
         return result;
     }
-    
+
     public static NFA oneOrMore(NFA nfa) {
-        
+
         NFA result = new NFA();
         result.addAllStates(nfa);
-        
+
         NFAState start = result.getStartState();
         start.addTransition(new EpsilonTransition(nfa.getStartState()));
-        
+
         NFAState childAccepting = nfa.getAcceptingState();
         NFAState resultAccepting = result.getAcceptingState();
         childAccepting.addTransition(new EpsilonTransition(resultAccepting));
         resultAccepting.addTransition(new EpsilonTransition(start));
-        
+
         return result;
     }
-    
+
     public static NFA zeroOrMore(NFA nfa) {
-        
+
         NFA result = new NFA();
         result.addAllStates(nfa);
-        
+
         NFAState start = result.getStartState();
         start.addTransition(new EpsilonTransition(nfa.getStartState()));
         start.addTransition(new EpsilonTransition(result.getAcceptingState()));
-        
+
         NFAState childAccepting = nfa.getAcceptingState();
         childAccepting.addTransition(new EpsilonTransition(start));
         childAccepting.addTransition(new EpsilonTransition(result.getAcceptingState()));
-        
+
         return result;
     }
-    
+
     public static NFA digit() {
         return NFA.acceptRange('0', '9');
     }
-    
+
     public static NFA nonZeroDigit(){
         return NFA.acceptRange('1', '9');
     }
@@ -97,12 +97,12 @@ public class NFA {
         NFAState start = result.getStartState();
         NFAState accepting = result.getAcceptingState();
         start.addTransition(new RangeTransition(first, last, accepting));
-        
+
         return result;
     }
 
     public static NFA letter() {
-        
+
         NFA result = new NFA();
         NFAState start = result.getStartState();
         NFAState accepting = result.getAcceptingState();
@@ -113,32 +113,32 @@ public class NFA {
         // (http://docs.oracle.com/javase/specs/jls/se5.0/html/lexical.html#3.8)
         start.addTransition(new CharacterTransition('_', accepting));
         start.addTransition(new CharacterTransition('$', accepting));
-        
+
         return result;
     }
-    
+
     public static NFA literal(String literal) {
-        
+
         NFA result = new NFA();
-        
+
         NFAState currentState = result.getStartState();
         for (int i = 0; i < literal.length(); i++) {
-            
+
             NFAState nextState = result.createState();
             currentState.addTransition(new CharacterTransition(literal.charAt(i), nextState));
             currentState = nextState;
         }
-        
+
         NFAState acceptingState = result.getAcceptingState();
         currentState.addTransition(new EpsilonTransition(acceptingState));
-        
+
         return result;
     }
-    
+
     public static NFA escapeSequence() {
         // escapeSequence -> \b|\t|\n|\f|\r|\"|\'|\\|(OctalEscape)
         // http://docs.oracle.com/javase/specs/jls/se5.0/html/lexical.html#101089
-        NFA result = NFA.concatenate(NFA.literal("\\"), 
+        NFA result = NFA.concatenate(NFA.literal("\\"),
                                      NFA.union(NFA.literal("b"),
                                                NFA.literal("t"),
                                                NFA.literal("n"),
@@ -162,18 +162,18 @@ public class NFA {
     }
 
     private final ArrayList<NFAState> states;
-    
+
     private NFA() {
         states = new ArrayList<NFAState>();
         createState();
         createAcceptingState();
     }
-    
+
     private void createAcceptingState() {
         NFAState state = new NFAState(true);
         addState(state);
     }
-    
+
     public NFAState getStartState() {
         return getState(0);
     }
@@ -181,50 +181,50 @@ public class NFA {
     public NFAState getAcceptingState() {
         return getState(1);
     }
-    
+
     public NFAState getState(int id) {
         return states.get(id);
     }
-    
+
     private Iterable<NFAState> getStates() {
         return states;
     }
-    
+
     public NFAState createState() {
         NFAState state = new NFAState();
         addState(state);
         return state;
     }
-    
+
     private void addState(NFAState state) {
         state.setParent(this);
         states.add(state);
     }
-    
+
     private void addAllStates(NFA nfa) {
         for (NFAState state : nfa.getStates()) {
             addState(state);
         }
     }
-    
+
     public int getId(NFAState state) {
         return states.indexOf(state);
     }
-    
+
     @Override
     public String toString() {
-        
+
         String result = "";
-        
+
         for (NFAState state : states) {
-            
+
             result += state.toString() + "\n";
-            
+
             for (NFATransition transition : state.getTransitions()) {
                 result += "\t" + transition.toString() + "\n";
             }
         }
-        
+
         return result;
     }
 }
