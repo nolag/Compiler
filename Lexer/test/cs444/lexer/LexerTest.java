6b6e8590a4fa06ea87874fab3a09b1de2959ed3e
Arturo Pie <arturotd08@yahoo.ca>
2013-01-17 23:38:49 -0500

    adds tests for identifier, keywords and Int literals. Also adds some usefull functions

--- Lexer/test/cs444/lexer/LexerTest.java
+++ Lexer/test/cs444/lexer/LexerTest.java
@@ -12,8 +12,6 @@ import org.junit.Test;
 import cs444.lexer.Token.Type;
 
 public class LexerTest {
-    BufferedReader reader;
-
     @Test
     public void testValidTraditionalComment() throws Exception {
        assertTokenFor("/* some comment */", Token.Type.COMMENT);
@@ -24,10 +22,30 @@ public class LexerTest {
         assertTokenFor("// some comment", Token.Type.END_LINE_COMMENT);
     }
 
+    @Test
+    public void testValidIdentifiers() throws Exception {
+        assertTokenFor("String", Token.Type.ID);
+        assertTokenFor("i3", Token.Type.ID);
+        assertTokenFor("MAX_VALUE", Token.Type.ID);
+        assertTokenFor("isLetterOrDigit", Token.Type.ID);
+    }
+
+    @Test
+    public void testValidKeywords() throws Exception {
+        assertTokenFor("if", Token.Type.IF);
+        assertTokenFor("implements", Token.Type.IMPLEMENTS);
+    }
+
+    @Test
+    public void testValidIntLiterals() throws Exception {
+        assertTokenFor("0", Token.Type.DECIMAL_INTEGER_LITERAL);
+        assertTokenFor("8", Token.Type.DECIMAL_INTEGER_LITERAL);
+        assertTokenFor("999990", Token.Type.DECIMAL_INTEGER_LITERAL);
+    }
+
     @After
     public void runAfterEveryTest() throws IOException {
-        if (null != reader && reader.markSupported())
-            reader.reset();
+        Lexer.reset();
     }
 
     private void assertTokenFor(String string, Type tokenType) throws Exception {
@@ -36,20 +54,29 @@ public class LexerTest {
         Token token = scanner.getNextToken();
         assertToken(tokenType, string, token);
         assertNull(scanner.getNextToken());
+        Lexer.reset();
     }
 
     private Lexer getScannerFor(String input) throws IOException {
-        reader = new BufferedReader(new StringReader(input));
-        reader.mark(input.length());
+        BufferedReader reader = new BufferedReader(new StringReader(input));
 
         Lexer.load(reader);
         return Lexer.getInstance();
     }
 
     private void assertToken(Token.Type type, String lexeme, Token token){
-        System.out.println(lexeme);
         String failMessage = "Unexpected token: " + token;
         assertEquals(failMessage, type, token.getType());
         assertEquals(failMessage, lexeme, token.getLexeme());
+    }
+
+    // useful for debugging
+    @SuppressWarnings("unused")
+    private void printAllTokensFor(String string) throws Exception{
+        Lexer scanner = getScannerFor(string);
+        Token token;
+        while (null != (token = scanner.getNextToken())) {
+            System.out.println(token.toString());
+        }
     }
 }
