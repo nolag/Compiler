83e0b4539b570e81089f8984165ea89b6f706560
Brandon Walderman <bdwalder@uwaterloo.ca>
2013-02-26 23:33:12 -0500

    Organized A2 marmoset test cases add modified acceptance test to search subfolders.

--- Compiler/tst/cs444/acceptance/Asst1.java
+++ Compiler/tst/cs444/acceptance/Asst1.java
@@ -1,15 +1,8 @@
 package cs444.acceptance;
 
-import static org.junit.Assert.assertEquals;
-
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.junit.Test;
-
-import cs444.Compiler;
 
 public class Asst1 {
 
@@ -17,74 +10,24 @@ public class Asst1 {
     public void testValidJoosCode() throws IOException, InterruptedException {
         String path = "JoosPrograms/SyntacticallyValidPrograms/";
 
-        assertReturnCodeForFiles(path, 0, true);
+        TestHelper.assertReturnCodeForFiles(path, 0, true);
     }
 
     @Test
     public void testInvalidJoosCode() throws IOException, InterruptedException {
         String path = "JoosPrograms/SyntacticallyInvalidPrograms/";
 
-        assertReturnCodeForFiles(path, 42, false);
+        TestHelper.assertReturnCodeForFiles(path, 42, false);
     }
 
     @Test
     public void testMarmosetTestCases() throws IOException, InterruptedException {
         String path = "JoosPrograms/MarmosetPrograms/a1/valid/";
 
-        assertReturnCodeForFiles(path, 0, false);
+        TestHelper.assertReturnCodeForFiles(path, 0, false);
 
         path = "JoosPrograms/MarmosetPrograms/a1/invalid/";
 
-        assertReturnCodeForFiles(path, 42, false);
-    }
-
-    private void assertReturnCodeForFiles(String path, int expectedReturnCode, boolean printErrors) throws IOException,
-                                                                                                           InterruptedException {
-        File folder = new File(path);
-
-        int totalTests = 0;
-        int filesSkipped = 0;
-        List<String> failFiles = new ArrayList<String>();
-        for (File file : folder.listFiles()) {
-            String fileName = file.getName();
-
-            // Use this line to test a single file
-            //if (!fileName.equals("J1_IntRange_MinNegativeInt.java")) continue;
-
-            if (file.isFile() && fileName.toLowerCase().endsWith(".java")){
-                if (compileFile(path + fileName, printErrors) == expectedReturnCode) {
-                    System.out.print(".");
-                }else{
-                    System.out.print("F");
-                    failFiles.add(path + fileName);
-                }
-                totalTests++;
-            }else{
-                System.out.print("*"); // skip file
-                filesSkipped++;
-            }
-        }
-
-        printSummary(totalTests, filesSkipped, failFiles);
-        int failures = failFiles.size();
-        assertEquals("Compiler return unexpected return code compiling " + failures + " files. Expected return code was: " + expectedReturnCode, 0, failures);
-    }
-
-    private void printSummary(int totalTests, int filesSkipped, List<String> failFiles) {
-        System.out.println("\nNumber of tests: " + totalTests);
-        if(filesSkipped > 0) System.out.println("Number of files skipped: " + filesSkipped);
-        if (failFiles.size() != 0){
-            System.out.println("Unexpected return code compiling these files: ");
-            for (String fileName: failFiles) {
-                System.out.println("\t" + fileName);
-            }
-        }
-    }
-
-    private int compileFile(String filePath, boolean printErrors) throws IOException,
-                                                                         InterruptedException {
-        String[] files = new String[1];
-        files[0] = filePath;
-        return Compiler.compile(files, printErrors);
+        TestHelper.assertReturnCodeForFiles(path, 42, false);
     }
 }
---
+++ Compiler/tst/cs444/acceptance/Asst2.java
@@ -0,0 +1,24 @@
+package cs444.acceptance;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class Asst2 {
+
+	@Test
+    public void testTypeCorrectJoosCode() throws IOException, InterruptedException {
+        TestHelper.assertReturnCodeForFiles("JoosPrograms/TypeCorrectPrograms/", 0, true);
+    }
+
+    @Test
+    public void testNonTypeCorrectJoosCode() throws IOException, InterruptedException {
+        TestHelper.assertReturnCodeForFiles("JoosPrograms/NonTypeCorrectPrograms/", 42, false);
+    }
+
+    @Test
+    public void testMarmosetTestCases() throws IOException, InterruptedException {
+        //TestHelper.assertReturnCodeForFiles("JoosPrograms/MarmosetPrograms/a2/valid/", 0, false);
+        //TestHelper.assertReturnCodeForFiles("JoosPrograms/MarmosetPrograms/a2/invalid/", 42, false);
+    }
+}
