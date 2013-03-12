085eeb1ae39dfc2412120b72d3fd44ed1b3e47e0
Arturo Pie <arturo.pie@gmail.com>
2013-03-12 19:33:06 -0400

    fixes tests due to a bad conflict

--- Compiler/src/cs444/parser/JoosASTBuilder.java
+++ Compiler/src/cs444/parser/JoosASTBuilder.java
@@ -37,7 +37,7 @@ public class JoosASTBuilder implements IASTBuilder {
             new ClassInterfaceFactory(), new FieldSymbolFactory(), new MethodSymbolFactory(), new FieldAccessSymbolFactory(),
             new LocalDclFactory(), new InterfaceMethodSymbolFactory(), new ConstructorSymbolFactory(),
             new MethodInvokeSymbolFactory(), new IntegerLiteralFactory(), new StringLiteralFactory(), new SimpleTerminalFactory(),
-            new ReturnExprFractory(), new UniOpExprFactory(), new BinOpFactory(), new CastExpressionFactory(), new CreationExprFactory()});
+            new ReturnExprFractory(), new UniOpExprFactory(), new BinOpFactory(), new CreationExprFactory(), new CastExpressionFactory()});
 
     public ISymbol build(String fileName, ANonTerminal start) throws OutOfRangeException, UnsupportedException, IllegalModifierException, InvalidFileNameException {
     	for(ASTSymbolFactory astSymbol : simplifications){
