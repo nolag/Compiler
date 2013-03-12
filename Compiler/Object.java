e7a4458278e1b7f927443e58e4d0e8b07dbdff2c
Arturo Pie <arturo.pie@gmail.com>
2013-03-12 17:20:50 -0400

    removes TypeCheckerVisitor

--- Compiler/Object.java
+++ Compiler/Object.java
@@ -4,9 +4,11 @@ public class Object{
     public Object(){}
 
     public Object f;
-    
 
-    public Object B(int a, int b){
-        return f.B(a, b).f.B(a + b, b);
+    public int B(int a1, int b1){
+       // return f.B(a, b).f.B(a + b, b);
+        Object a = null;
+        a = (int [])a;
+        return 123;
     }
 }
