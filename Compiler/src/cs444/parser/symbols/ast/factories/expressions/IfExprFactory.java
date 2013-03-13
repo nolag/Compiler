cf919f245a934b13eac1075b0d73b439a27a1f1a
Arturo Pie <arturo.pie@gmail.com>
2013-03-13 17:48:09 -0400

    removes unused import

--- Compiler/src/cs444/parser/symbols/ast/factories/expressions/IfExprFactory.java
+++ Compiler/src/cs444/parser/symbols/ast/factories/expressions/IfExprFactory.java
@@ -4,7 +4,6 @@ import cs444.parser.symbols.ANonTerminal;
 import cs444.parser.symbols.ISymbol;
 import cs444.parser.symbols.JoosNonTerminal;
 import cs444.parser.symbols.ast.expressions.IfExprSymbol;
-import cs444.parser.symbols.ast.expressions.WhileExprSymbol;
 import cs444.parser.symbols.ast.factories.ASTSymbolFactory;
 import cs444.parser.symbols.exceptions.IllegalModifierException;
 import cs444.parser.symbols.exceptions.OutOfRangeException;
