363f2100595db62c3c01854772c28df1af732ac5
Arturo Pie <arturo.pie@gmail.com>
2013-03-13 00:22:29 -0400

    fixes more bugs in while. It probably needs to extract a decorator

--- Compiler/src/cs444/parser/symbols/ast/expressions/WhileExprSymbol.java
+++ Compiler/src/cs444/parser/symbols/ast/expressions/WhileExprSymbol.java
@@ -4,8 +4,14 @@ import java.util.List;
 
 import cs444.CompilerException;
 import cs444.ast.ISymbolVisitor;
+import cs444.parser.symbols.ANonTerminal;
 import cs444.parser.symbols.ISymbol;
+import cs444.parser.symbols.ast.BooleanLiteralSymbol;
+import cs444.parser.symbols.ast.MethodInvokeSymbol;
+import cs444.parser.symbols.ast.MethodSymbol;
+import cs444.parser.symbols.ast.NameSymbol;
 import cs444.parser.symbols.ast.Typeable;
+import cs444.parser.symbols.exceptions.UnexpectedTokenException;
 
 public class WhileExprSymbol extends BaseExprSymbol {
 
@@ -18,11 +24,41 @@ public class WhileExprSymbol extends BaseExprSymbol {
         return (Typeable) children.get(0);
     }
 
+    public ANonTerminal getBody() {
+        return (ANonTerminal) children.get(1);
+    }
+
+    public BaseExprSymbol getConditionExpression() {
+        return (BaseExprSymbol) children.get(0);
+    }
+
     @Override
     public void accept(ISymbolVisitor visitor) throws CompilerException {
         visitor.open(this);
         visitor.prepareCondition(getCondition());
         visitor.visit(this);
+        conditionAccept(visitor);
+        getBody().accept(visitor);
         visitor.close(this);
+    }
+
+    private void conditionAccept(ISymbolVisitor visitor) throws CompilerException {
+
+        // TODO pull this to a Decorator????
+        ISymbol iSymBody = children.get(0);
+
+        if(iSymBody instanceof BaseExprSymbol){
+            ((BaseExprSymbol)iSymBody).accept(visitor);
+        }else if(iSymBody instanceof NameSymbol){
+            ((NameSymbol) iSymBody).accept(visitor);
+        }else if (iSymBody instanceof BooleanLiteralSymbol){
+            ((BooleanLiteralSymbol) iSymBody).accept(visitor);
+        }else if ((iSymBody) instanceof MethodInvokeSymbol){
+            ((MethodInvokeSymbol) iSymBody).accept(visitor);
+        }else{
+            String errorMsg = "Unexpected while's body. Probably a bug?. Got symbol: " + iSymBody.getName();
+            System.err.println(errorMsg);
+            throw new CompilerException(errorMsg);
+        }
     }
 }
