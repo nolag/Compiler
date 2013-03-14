a1aef727ef5343e51879a5a9d440ab70710c80d9
Arturo Pie <arturo.pie@gmail.com>
2013-03-14 11:11:10 -0400

    initializes variable in testing file Object.java

--- Compiler/Object.java
+++ Compiler/Object.java
@@ -8,7 +8,7 @@ public class Object{
     public int B(int a1, int b1){
        // return f.B(a, b).f.B(a + b, b);
         int i = 0;
-        int j;
+        int j = 2;
         for (i = 2; i < 10; j = i + 1){
             return 123;
         }
