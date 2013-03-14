11d3b4e135ed0f2e62b6bd9e3b7b4ee1d300f6ad
Arturo Pie <arturo.pie@gmail.com>
2013-03-14 16:55:04 -0400

    adds numeric add op

--- Compiler/src/cs444/types/LocalDclLinker.java
+++ Compiler/src/cs444/types/LocalDclLinker.java
@@ -28,6 +28,7 @@ import cs444.parser.symbols.ast.ThisSymbol;
 import cs444.parser.symbols.ast.TypeSymbol;
 import cs444.parser.symbols.ast.Typeable;
 import cs444.parser.symbols.ast.TypeableTerminal;
+import cs444.parser.symbols.ast.expressions.AddExprSymbol;
 import cs444.parser.symbols.ast.expressions.AndExprSymbol;
 import cs444.parser.symbols.ast.expressions.ArrayAccessExprSymbol;
 import cs444.parser.symbols.ast.expressions.AssignmentExprSymbol;
@@ -246,8 +247,11 @@ if(checkTypes){
         currentTypes.peek().add(typeableDeque.getLast());
     }
 
-    private void simpleVistorHelper(TypeableTerminal tt, String visitorType){
+    private void simpleVistorHelper(TypeableTerminal tt, String visitorType) throws UndeclaredException{
         APkgClassResolver resolver = PkgClassInfo.instance.getSymbol(visitorType);
+        if (resolver == null) {
+            throw new UndeclaredException(visitorType, PkgClassResolver.generateUniqueName(currentMC, currentMC.dclName));
+        }
         TypeSymbol type = new TypeSymbol(resolver.fullName, false, false);
         type.setTypeDclNode(resolver);
         tt.setType(type);
@@ -255,38 +259,38 @@ if(checkTypes){
     }
 
     @Override
-    public void visit(NullSymbol nullSymbol) {
+    public void visit(NullSymbol nullSymbol) throws UndeclaredException {
         simpleVistorHelper(nullSymbol, JoosNonTerminal.NULL);
     }
 
     @Override
-    public void visit(IntegerLiteralSymbol intSymbol) {
+    public void visit(IntegerLiteralSymbol intSymbol) throws UndeclaredException {
         simpleVistorHelper(intSymbol, JoosNonTerminal.INTEGER);
 
     }
 
     @Override
-    public void visit(CharacterLiteralSymbol characterSymbol) {
+    public void visit(CharacterLiteralSymbol characterSymbol) throws UndeclaredException {
         simpleVistorHelper(characterSymbol, JoosNonTerminal.CHAR);
     }
 
     @Override
-    public void visit(BooleanLiteralSymbol boolSymbol){
+    public void visit(BooleanLiteralSymbol boolSymbol) throws UndeclaredException{
         simpleVistorHelper(boolSymbol, JoosNonTerminal.BOOLEAN);
     }
 
     @Override
-    public void visit(StringLiteralSymbol stringSymbol){
+    public void visit(StringLiteralSymbol stringSymbol) throws UndeclaredException{
         simpleVistorHelper(stringSymbol, JoosNonTerminal.STRING);
     }
 
     @Override
-    public void visit(EmptyStatementSymbol emptySymbol){
+    public void visit(EmptyStatementSymbol emptySymbol) throws CompilerException{
         simpleVistorHelper(emptySymbol, JoosNonTerminal.VOID);
     }
 
     @Override
-    public void visit(ThisSymbol thisSymbol) {
+    public void visit(ThisSymbol thisSymbol) throws UndeclaredException {
         simpleVistorHelper(thisSymbol, enclosingClassName);
     }
 
@@ -527,42 +531,49 @@ if(checkTypes){
     }
 
     @Override
-    public void visit(LtExprSymbol op) throws IllegalCastAssignmentException, UndeclaredException, BadOperandsTypeException  {
+    public void visit(LtExprSymbol op) throws UndeclaredException, BadOperandsTypeException  {
 if(checkTypes){
         bothIntHelper(JoosNonTerminal.BOOLEAN);
 }
     }
 
     @Override
-    public void visit(LeExprSymbol op) throws IllegalCastAssignmentException, UndeclaredException, BadOperandsTypeException  {
+    public void visit(LeExprSymbol op) throws UndeclaredException, BadOperandsTypeException  {
 if(checkTypes){
         bothIntHelper(JoosNonTerminal.BOOLEAN);
 }
     }
 
     @Override
-    public void visit(SubtractExprSymbol op) throws IllegalCastAssignmentException, UndeclaredException, BadOperandsTypeException  {
+    public void visit(AddExprSymbol op) throws BadOperandsTypeException, UndeclaredException {
 if(checkTypes){
         bothIntHelper(JoosNonTerminal.INTEGER);
 }
     }
 
     @Override
-    public void visit(MultiplyExprSymbol op) throws IllegalCastAssignmentException, UndeclaredException, BadOperandsTypeException  {
+    public void visit(SubtractExprSymbol op) throws UndeclaredException, BadOperandsTypeException  {
 if(checkTypes){
         bothIntHelper(JoosNonTerminal.INTEGER);
 }
     }
 
     @Override
-    public void visit(DivideExprSymbol op) throws IllegalCastAssignmentException, UndeclaredException, BadOperandsTypeException  {
+    public void visit(MultiplyExprSymbol op) throws UndeclaredException, BadOperandsTypeException  {
 if(checkTypes){
         bothIntHelper(JoosNonTerminal.INTEGER);
 }
     }
 
     @Override
-    public void visit(RemainderExprSymbol op) throws IllegalCastAssignmentException, UndeclaredException, BadOperandsTypeException  {
+    public void visit(DivideExprSymbol op) throws UndeclaredException, BadOperandsTypeException  {
+if(checkTypes){
+        bothIntHelper(JoosNonTerminal.INTEGER);
+}
+    }
+
+    @Override
+    public void visit(RemainderExprSymbol op) throws UndeclaredException, BadOperandsTypeException  {
 if(checkTypes){
         bothIntHelper(JoosNonTerminal.INTEGER);
 }
