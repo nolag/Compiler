36f1ab7afadd491e6139b86ae51d27e6fb31d19b
Arturo Pie <arturo.pie@gmail.com>
2013-03-14 11:06:15 -0400

    finishes testing arith op

--- Compiler/JoosPrograms/A3/invalid/InvArithOper/InvArithOper.java
+++ Compiler/JoosPrograms/A3/invalid/InvArithOper/InvArithOper.java
@@ -2,9 +2,9 @@ public class InvArithOper{
     public InvArithOper(){ }
 
     public void doStuff(){
-//        int a = false - 5;
-        int b = 2 * true;
+        int a = false - 5;
+//        int b = 2 * true;
 //        int = "1" / 2;
-//        int d = 4 % "nooo"; // TODO: doesn't work when String is second operand
+//        int d = 4 % "nooo"; // TODO: doesn't work coz String Literal is not been type checked
     }
 }
