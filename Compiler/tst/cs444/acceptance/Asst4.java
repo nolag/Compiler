4f48f4ce10f5e17592606832163ba3b007871152
Arturo Pie <arturo.pie@gmail.com>
2013-03-17 17:44:21 -0400

    runs A4 invalid test as well

--- Compiler/tst/cs444/acceptance/Asst4.java
+++ Compiler/tst/cs444/acceptance/Asst4.java
@@ -2,7 +2,6 @@ package cs444.acceptance;
 
 import java.io.IOException;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class Asst4 {
@@ -12,7 +11,6 @@ public class Asst4 {
         TestHelper.assertReturnCodeForFiles("JoosPrograms/MarmosetPrograms/a4/valid/", 0, true);
     }
 
-    @Ignore("not ready")
     @Test
     public void testMarmosetInvalidTestCases() throws IOException, InterruptedException{
         TestHelper.assertReturnCodeForFiles("JoosPrograms/MarmosetPrograms/a4/invalid/", 42,  false);
