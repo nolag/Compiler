bf9e65d3315f90b676d5fff50d4b65dff201bbea
Arturo Pie <arturo.pie@gmail.com>
2013-03-28 12:22:15 -0400

    checks for 0 before deviding in const expression evaluaction

--- Compiler/src/cs444/parser/symbols/ast/expressions/RemainderExprSymbol.java
+++ Compiler/src/cs444/parser/symbols/ast/expressions/RemainderExprSymbol.java
@@ -48,7 +48,7 @@ public class RemainderExprSymbol extends BinOpExpr {
                 leftOperand instanceof INumericLiteral){
             int val1 = ((INumericLiteral)leftOperand).getValue();
             int val2 = ((INumericLiteral)rightOperand).getValue();
-            return new IntegerLiteralSymbol(val1 % val2);
+            return (val2 == 0)? null : new IntegerLiteralSymbol(val1 % val2);
         }else{
             return null;
         }
