f9a178f6d9ebfc0a5539cab45f0d46d8129b03aa
Ryan Tinianov <rtinianov@uwaterloo.ca>
2013-04-02 09:40:40 -0400

    forgot to uncomment not ready

--- Compiler/a5.tst/cs444/acceptance/A5.java
+++ Compiler/a5.tst/cs444/acceptance/A5.java
@@ -7,7 +7,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class A5 {
@@ -19,7 +18,7 @@ public class A5 {
                 true, ignoreList, new AsmAndLinkCallback());
     }
 
-    @Ignore("not ready")
+    //@Ignore("not ready")
     @Test
     public void testMarmosetPrograms() throws IOException, InterruptedException{
         List<String> ignoreList = new LinkedList<String>(Arrays.asList(
