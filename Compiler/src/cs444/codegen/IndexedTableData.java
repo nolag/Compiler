3b31b5e93bbf76f0f9c540634d74c1398e9e63f9
Arturo Pie <arturo.pie@gmail.com>
2013-04-02 11:17:33 -0400

    extract table code from SelectorIdexedTable

---
+++ Compiler/src/cs444/codegen/IndexedTableData.java
@@ -0,0 +1,99 @@
+package cs444.codegen;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import cs444.codegen.instructions.Comment;
+import cs444.codegen.instructions.Dd;
+import cs444.codegen.instructions.Extern;
+import cs444.codegen.instructions.Global;
+import cs444.codegen.instructions.Instruction;
+import cs444.codegen.instructions.Label;
+import cs444.codegen.instructions.Section;
+import cs444.codegen.instructions.Section.SectionType;
+
+public class IndexedTableData {
+    public Map<String, Map<String, String>> indexedTable = new HashMap<String, Map<String,String>>();
+    public Map<String, Integer> offset = new LinkedHashMap<String, Integer>();
+    public int offsetCounter = 0;
+    private List<Instruction> instructions = new LinkedList<Instruction>();
+    private int cellDataSize;
+
+
+    public IndexedTableData(int cellDataSize) {
+        this.cellDataSize = cellDataSize;
+    }
+
+    public void addIndex(String columnName, String rowName,
+            String cellData) {
+        Map<String, String> column = this.addColumn(columnName);
+
+        column.put(rowName, cellData);
+    }
+
+    public Map<String, String> addColumn(String columnName) {
+        Map<String, String> column = indexedTable.get(columnName);
+        if (column == null){
+            column = new HashMap<String, String>();
+            indexedTable.put(columnName, column);
+        }
+        return column;
+    }
+
+    public void addRow(String rowName) {
+        if (!this.offset.containsKey(rowName)){
+            this.offset.put(rowName, this.offsetCounter);
+            this.offsetCounter += cellDataSize;
+        }
+    }
+
+    public int getOffset(String rowName) {
+        return this.offset.get(rowName);
+    }
+
+
+    public void genCode() {
+        instructions.add(new Section(SectionType.DATA));
+
+        for (String colHeaderLabel : indexedTable.keySet()) {
+            instructions.add(new Global(colHeaderLabel));
+            Map<String, String> column = indexedTable.get(colHeaderLabel);
+
+            for (String implLabel : column.values()) {
+                instructions.add(new Extern(implLabel));
+            }
+            instructions.add(new Label(colHeaderLabel));
+
+            if (column.isEmpty()){
+                instructions.add(new Comment(colHeaderLabel + " does not have non-static method, so add NULL"));
+                instructions.add(new Dd(Immediate.NULL));
+            }
+
+            for (String selector : offset.keySet()) {
+                String implLabel = column.get(selector);
+                if (implLabel == null){
+                    instructions.add(new Comment(colHeaderLabel + " does not have access to " + selector + ":"));
+                    instructions.add(new Dd(Immediate.NULL));
+                }else{
+                    instructions.add(new Dd(new Immediate(implLabel)));
+                }
+            }
+        }
+    }
+
+    public void printCodeToFile(String filePath) throws IOException {
+        File file = new File(filePath);
+        file.createNewFile();
+        PrintStream printer = new PrintStream(file);
+
+        for(Instruction instruction : instructions) printer.println(instruction.generate());
+
+        printer.close();
+    }
+}
--- Compiler/src/cs444/codegen/SelectorIndexedTable.java
+++ Compiler/src/cs444/codegen/SelectorIndexedTable.java
@@ -1,88 +1,30 @@
 package cs444.codegen;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.PrintStream;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-import cs444.codegen.instructions.Comment;
-import cs444.codegen.instructions.Dd;
-import cs444.codegen.instructions.Extern;
-import cs444.codegen.instructions.Global;
-import cs444.codegen.instructions.Instruction;
-import cs444.codegen.instructions.Label;
-import cs444.codegen.instructions.Section;
-import cs444.codegen.instructions.Section.SectionType;
+import cs444.codegen.InstructionArg.Size;
 import cs444.types.APkgClassResolver;
 
 public class SelectorIndexedTable {
-    private final Map<String, Map<String, String>> sit = new HashMap<String, Map<String,String>>();;
-    private final Map<String, Integer> offset = new LinkedHashMap<String, Integer>();
-    private int offsetCounter = 0;
+    private IndexedTableData table = new IndexedTableData(SizeHelper.getIntSize(Size.DWORD));
 
     public void addIndex(String className, String selector, String methodImplLabel) {
-
-        Map<String, String> column = this.addClass(className);
-
-        column.put(selector, methodImplLabel);
+        table.addIndex(className, selector, methodImplLabel);
     }
 
     public Map<String, String> addClass(String fullName) {
-        Map<String, String> column = sit.get(fullName);
-        if (column == null){
-            column = new HashMap<String, String>();
-            sit.put(fullName, column);
-        }
-        return column;
+        return table.addColumn(fullName);
     }
 
     public void addSelector(String selector) {
-        if (!offset.containsKey(selector)){
-            offset.put(selector, offsetCounter);
-            offsetCounter += 4;
-        }
+        table.addRow(selector);
     }
 
     public int getOffset(String selector){
-        return offset.get(selector);
-    }
-
-    public void genCodeAndPrint(PrintStream printer) {
-        List<Instruction> instructions = new LinkedList<Instruction>();
-
-        instructions.add(new Section(SectionType.DATA));
-
-        for (String classLabel : sit.keySet()) {
-            instructions.add(new Global(classLabel));
-            Map<String, String> column = sit.get(classLabel);
-
-            for (String implLabel : column.values()) {
-                instructions.add(new Extern(implLabel));
-            }
-            instructions.add(new Label(classLabel));
-
-            if (column.isEmpty()){
-                instructions.add(new Comment(classLabel + " does not have non-static method, so add NULL"));
-                instructions.add(new Dd(Immediate.NULL));
-            }
-
-            for (String selector : offset.keySet()) {
-                String implLabel = column.get(selector);
-                if (implLabel == null){
-                    instructions.add(new Comment(classLabel + " does not have access to " + selector + ":"));
-                    instructions.add(new Dd(Immediate.NULL));
-                }else{
-                    instructions.add(new Dd(new Immediate(implLabel)));
-                }
-            }
-        }
-
-        for(Instruction instruction : instructions) printer.println(instruction.generate());
+        return table.getOffset(selector);
     }
 
     public static SelectorIndexedTable generateSIT(List<APkgClassResolver> resolvers, boolean outputFile, String directory)
@@ -94,12 +36,10 @@ public class SelectorIndexedTable {
             resolver.addToSelectorIndexedTable(sit);
         }
 
+        sit.table.genCode();
+
         if(outputFile){
-            File file = new File(directory + "_joos.sit.s");
-            file.createNewFile();
-            PrintStream printer = new PrintStream(file);
-            sit.genCodeAndPrint(printer);
-            printer.close();
+            sit.table.printCodeToFile(directory + "_joos.sit.s");
         }
 
         return sit;
