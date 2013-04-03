7d6a0c18452752e7170e11f33971a37cb08c005e
Ryan Tinianov <rtinianov@uwaterloo.ca>
2013-04-03 19:57:04 -0400

    changed StringConcat to pass

--- Compiler/tst/cs444/acceptance/TestHelper.java
+++ Compiler/tst/cs444/acceptance/TestHelper.java
@@ -31,7 +31,7 @@ public class TestHelper {
 			String fileName = file.getName();
 
 			// Use this line to test a single file
-			//if (!fileName.equals("J1_A_StringConstAEQ_ANE")) continue;
+			//if (!fileName.equals("StringConcat")) continue;
 
 			if (ignoreList.contains(fileName)){
 			    System.out.print("*"); // skip file
