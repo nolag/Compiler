c678f33aba146d6aeeb8d7b0183d36630e7dc269
Ryan Tinianov <rtinianov@uwaterloo.ca>
2013-04-03 22:54:55 -0400

    Added new test case

--- Compiler/JoosPrograms/A5/NoStdLibPrograms/StringAddTest/java/lang/Object.java
+++ Compiler/JoosPrograms/A5/NoStdLibPrograms/StringAddTest/java/lang/Object.java
@@ -10,20 +10,21 @@ public class Object{
     public static int test(){
         String s1 = "lol";
         String s2 = "lol";
-        String s3 = s1 + s2;
+        //String s3 = s1 + s2;
+        String s3 = s1.concat(s2);
         int i = 0;
         
         //using binary bits so it's easy to see what is wrong.
         if(!s3.equals((Object)"lollol")) i = i + 1;
         
-        s3 = "lol" + 7;
+        /*s3 = "lol" + 7;
         if(!s3.equals((Object)"lol7")) i = i + 2;
         
         s3 = 7 + "rofl";
         if(!s3.equals((Object)"7rofl")) i = i + 4;
         
         s3 = "lol" + 7 + "rofl";
-        if(!s3.equals((Object)"lol7rofl")) i = i + 8;
+        if(!s3.equals((Object)"lol7rofl")) i = i + 8;*/
         
         return i;
     }
