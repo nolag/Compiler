d9e2130bb46d82d0b1fc94967b75dd0451979c0f
Ryan Tinianov <rtinianov@uwaterloo.ca>
2013-04-04 21:17:34 -0400

    Updated failing list to the 17 failing tests

--- Compiler/a5.tst/cs444/acceptance/A5.java
+++ Compiler/a5.tst/cs444/acceptance/A5.java
@@ -32,23 +32,19 @@ public class A5 {
                 "J1_A_ConcatInStaticInvoke",
                 "J1_A_String_ByteShortCharInt",
                 "J1_Hello",
-                "J1_WildConcat",
                 "J1_charadd",
-                "J1_concatInMethods",
                 "J1_concat_in_binop",
-                "J1_constructorbodycast",
                 "J1_implicitstringconcatenation",
                 "J1_intstringadd",
-                "J1_nestedcast",
                 "J1_sideeffects_array",
                 "J1_sideeffects_array2",
                 "J1_sideeffects_array3",
-                "J1_sideeffects_obj2",
                 "J1_sideeffects_obj",
+                "J1_sideeffects_obj2",
                 "J1_sideeffects_obj3",
                 "J1_sim_xor",
-                "J1_stringadd",
-                "J1_stringconcat"
+                "J1_stringconcat",
+                "J1_typecheck_plus"
                 ));
 
         Map<String, String> env = System.getenv();
