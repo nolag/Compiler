db722dfa8ea9b736b4479774d2228b5c7028682e
Ryan Tinianov <rtinianov@uwaterloo.ca>
2013-05-06 00:20:31 -0400

    forgot to set the compile directory back

--- Compiler/src/cs444/Compiler.java
+++ Compiler/src/cs444/Compiler.java
@@ -28,8 +28,8 @@ import cs444.types.APkgClassResolver;
 import cs444.types.PkgClassInfo;
 
 public class Compiler {
-    public static final String BASE_DIRECTORY = "/mnt/hgfs/RAM/";
-    //public static final String BASE_DIRECTORY = "";
+    //public static final String BASE_DIRECTORY = "/mnt/hgfs/RAM/";
+    public static final String BASE_DIRECTORY = "";
     public static final String OUTPUT_DIRECTORY = BASE_DIRECTORY + "output/";
 
     public static final int COMPILER_ERROR_CODE = 42;
