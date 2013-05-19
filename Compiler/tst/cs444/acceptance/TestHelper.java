2e66aa204887015c83db72595a8aa5ae4ce3ae5f
Ryan Tinianov <rtinianov@uwaterloo.ca>
2013-05-19 19:43:48 -0400

    removed duplicate tests

--- Compiler/tst/cs444/acceptance/TestHelper.java
+++ Compiler/tst/cs444/acceptance/TestHelper.java
@@ -34,7 +34,7 @@ public class TestHelper {
             final String fileName = file.getName();
 
             // Use this line to test a single file
-            //if (!fileName.equals("SmallStringTest")) continue;
+            //if (!fileName.equals("J1_NamedTypeArray.java")) continue;
 
             if (ignoreList.contains(fileName)){
                 System.out.print("*"); // skip file
