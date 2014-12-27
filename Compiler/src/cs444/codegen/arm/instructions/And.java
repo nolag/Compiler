778604efcd81756629d790d252c939e2f1cf690b
Ryan Tinianov <tinianov@live.com>
2014-12-27 13:23:11 -0800

    Makes divide and remainder, since ARM won't normally have it

--- Compiler/src/cs444/codegen/arm/instructions/And.java
+++ Compiler/src/cs444/codegen/arm/instructions/And.java
@@ -1,7 +1,7 @@
 package cs444.codegen.arm.instructions;
 
 import cs444.codegen.SizeHelper;
-import cs444.codegen.arm.Conditional;
+import cs444.codegen.arm.instructions.bases.Branch.Condition;
 import cs444.codegen.arm.Size;
 import cs444.codegen.arm.Operand2;
 import cs444.codegen.arm.Register;
@@ -9,7 +9,7 @@ import cs444.codegen.arm.instructions.bases.ArmInstruction;
 import cs444.codegen.arm.instructions.bases.DataProcessingWithDestination;
 
 public class And extends DataProcessingWithDestination {
-    public And(final boolean s, final Conditional when, final Register dest, final Register rhs, 
+    public And(final boolean s, final Condition when, final Register dest, final Register rhs, 
             final Operand2 lhs, final SizeHelper<ArmInstruction, Size> sizeHelper) {
         super("and", s, when, dest, rhs, lhs, sizeHelper);
     }
@@ -23,7 +23,7 @@ public class And extends DataProcessingWithDestination {
         super("and", s, dest, rhs, lhs, sizeHelper);
     }
     
-    public And(final Conditional when, final Register dest, final Register rhs, 
+    public And(final Condition when, final Register dest, final Register rhs, 
             final Operand2 lhs, final SizeHelper<ArmInstruction, Size> sizeHelper) {
         super("and", when, dest, rhs, lhs, sizeHelper);
     }
--- Compiler/src/cs444/codegen/arm/instructions/Sub.java
+++ Compiler/src/cs444/codegen/arm/instructions/Sub.java
@@ -1,7 +1,7 @@
 package cs444.codegen.arm.instructions;
 
 import cs444.codegen.SizeHelper;
-import cs444.codegen.arm.Conditional;
+import cs444.codegen.arm.instructions.bases.Branch.Condition;
 import cs444.codegen.arm.Size;
 import cs444.codegen.arm.Operand2;
 import cs444.codegen.arm.Register;
@@ -9,7 +9,7 @@ import cs444.codegen.arm.instructions.bases.ArmInstruction;
 import cs444.codegen.arm.instructions.bases.DataProcessingWithDestination;
 
 public class Sub extends DataProcessingWithDestination {
-    public Sub(final boolean s, final Conditional when, final Register dest, final Register rhs, 
+    public Sub(final boolean s, final Condition when, final Register dest, final Register rhs, 
             final Operand2 lhs, final SizeHelper<ArmInstruction, Size> sizeHelper) {
         super("sub", s, when, dest, rhs, lhs, sizeHelper);
     }
@@ -23,7 +23,7 @@ public class Sub extends DataProcessingWithDestination {
         super("sub", s, dest, rhs, lhs, sizeHelper);
     }
     
-    public Sub(final Conditional when, final Register dest, final Register rhs, 
+    public Sub(final Condition when, final Register dest, final Register rhs, 
             final Operand2 lhs, final SizeHelper<ArmInstruction, Size> sizeHelper) {
         super("sub", when, dest, rhs, lhs, sizeHelper);
     }
