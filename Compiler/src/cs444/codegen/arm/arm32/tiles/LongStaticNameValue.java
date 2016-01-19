090245340c6f03ba8b9eb685b5479b0e67a91d19
Ryan Tinianov <tinianov@live.com>
2016-01-18 18:04:09 -0800

    fix long fields test, need to run all then hopefully merge :)

--- Compiler/src/cs444/codegen/arm/arm32/tiles/LongStaticNameValue.java
+++ Compiler/src/cs444/codegen/arm/arm32/tiles/LongStaticNameValue.java
@@ -1,7 +1,8 @@
-package cs444.codegen.arm.tiles;
+package cs444.codegen.arm.arm32.tiles;
 
 import cs444.codegen.Platform;
 import cs444.codegen.SizeHelper;
+import cs444.codegen.arm.Immediate8;
 import cs444.codegen.arm.ImmediateStr;
 import cs444.codegen.arm.Register;
 import cs444.codegen.arm.Size;
@@ -10,17 +11,17 @@ import cs444.codegen.arm.instructions.Ldr;
 import cs444.codegen.arm.instructions.Movt;
 import cs444.codegen.arm.instructions.Movw;
 import cs444.codegen.arm.instructions.bases.ArmInstruction;
-import cs444.codegen.generic.tiles.helpers.NumericHelperTile;
+import cs444.codegen.generic.tiles.helpers.LongOnlyTile;
 import cs444.codegen.tiles.InstructionsAndTiming;
 import cs444.parser.symbols.ast.DclSymbol;
 import cs444.parser.symbols.ast.cleanup.SimpleNameSymbol;
 import cs444.types.PkgClassResolver;
 
-public class StaticNameValue extends NumericHelperTile<ArmInstruction, Size, SimpleNameSymbol> {
-    private static StaticNameValue tile;
+public class LongStaticNameValue extends LongOnlyTile<ArmInstruction, Size, SimpleNameSymbol> {
+    private static LongStaticNameValue tile;
 
-    public static StaticNameValue getTile() {
-        if (tile == null) tile = new StaticNameValue();
+    public static LongStaticNameValue getTile() {
+        if (tile == null) tile = new LongStaticNameValue();
         return tile;
     }
 
@@ -36,10 +37,12 @@ public class StaticNameValue extends NumericHelperTile<ArmInstruction, Size, Sim
         final DclSymbol dcl = name.dcl;
         final String staticFieldLbl = PkgClassResolver.getUniqueNameFor(dcl);
 
-        instructions.add(new Comment("Moving " + staticFieldLbl + " into R0"));
+        instructions.add(new Comment("Moving " + staticFieldLbl + " into R0, R2"));
         instructions.add(new Movw(Register.R0, new ImmediateStr(":lower16:" + staticFieldLbl), sizeHelper));
         instructions.add(new Movt(Register.R0, new ImmediateStr(":upper16:" + staticFieldLbl), sizeHelper));
+        instructions.add(new Ldr(Register.R2, Register.R0, Immediate8.FOUR, sizeHelper));
         instructions.add(new Ldr(Register.R0, Register.R0, sizeHelper));
+
         return instructions;
     }
 }
