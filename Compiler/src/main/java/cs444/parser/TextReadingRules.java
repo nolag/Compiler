810fa1d370469ea18cb66b06d770397d68c503e0
Ryan Tinianov <tinianov@live.com>
2019-02-24 17:47:41 -0800

    Read from resource as stream so it will work in a jar file

--- Compiler/src/main/java/cs444/parser/TextReadingRules.java
+++ Compiler/src/main/java/cs444/parser/TextReadingRules.java
@@ -4,24 +4,11 @@ import cs444.parser.symbols.SymbolState;
 import cs444.parser.symbols.factories.JoosNonTerminalFactory;
 import cs444.parser.symbols.factories.NonTerminalFactory;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
 public class TextReadingRules implements IParserRule {
-    private final File readFrom;
-
-    public TextReadingRules() {
-        readFrom = getJoosRuleFile();
-    }
-
-    private static File getJoosRuleFile() {
-        return new File(TextReadingRules.class.getClassLoader().getResource("JoosRules.txt").getFile());
-    }
-
     private Map<String, SymbolState> getRule(Map<Integer, Map<String, SymbolState>> rules, int number) {
         if (rules.containsKey(number)) {
             return rules.get(number);
@@ -33,34 +20,33 @@ public class TextReadingRules implements IParserRule {
 
     @Override
     public Map<Integer, Map<String, SymbolState>> getRules() throws IOException {
-        Map<Integer, Map<String, SymbolState>> rules = new HashMap<Integer, Map<String, SymbolState>>();
+        Map<Integer, Map<String, SymbolState>> rules = new HashMap<>();
 
-        BufferedReader reader = new BufferedReader(new FileReader(readFrom));
 
-        Map<String, NonTerminalFactory> factories = new HashMap<String, NonTerminalFactory>();
-
-        String line;
-        while ((line = reader.readLine()) != null) {
-            String[] split = line.split(" ");
-            SymbolState symbolState;
-            switch (split.length) {
-                case 1:
-                    factories.put(line, new JoosNonTerminalFactory(line));
-                    break;
-                case 3:
-                    symbolState = new SymbolState(Integer.parseInt(split[2]));
-                    getRule(rules, Integer.parseInt(split[0])).put(split[1], symbolState);
-                    break;
-                case 4:
-                    symbolState = new SymbolState(factories.get(split[2]), Integer.parseInt(split[3]));
-                    getRule(rules, Integer.parseInt(split[0])).put(split[1], symbolState);
-                    break;
-                default:
-                    System.err.println("Rule " + line + " is not understood, skipping");
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(TextReadingRules.class.getClassLoader().getResourceAsStream("JoosRules.txt")))) {
+            Map<String, NonTerminalFactory> factories = new HashMap<>();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] split = line.split(" ");
+                SymbolState symbolState;
+                switch (split.length) {
+                    case 1:
+                        factories.put(line, new JoosNonTerminalFactory(line));
+                        break;
+                    case 3:
+                        symbolState = new SymbolState(Integer.parseInt(split[2]));
+                        getRule(rules, Integer.parseInt(split[0])).put(split[1], symbolState);
+                        break;
+                    case 4:
+                        symbolState = new SymbolState(factories.get(split[2]), Integer.parseInt(split[3]));
+                        getRule(rules, Integer.parseInt(split[0])).put(split[1], symbolState);
+                        break;
+                    default:
+                        System.err.println("Rule " + line + " is not understood, skipping");
+                }
             }
         }
-
-        reader.close();
 
         return rules;
     }
